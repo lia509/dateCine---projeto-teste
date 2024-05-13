0b463a69731b31947ddd930f9682e52246a0f718
Giulia Marques <giuliamarques.564@gmail.com>
2024-05-13 11:41:08 -0300

    criação de tabelas

---
+++ src/db/database.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE dateCine;
+
+\c dateCine;
+
+CREATE TABLE produtores (
+    produtor_id INT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    nacionalidade VARCHAR(20) NOT NULL,
+    descrição VARCHAR(70) NOT NULL,
+    id_filme INT,
+    FOREIGN KEY (id_filme) REFERENCES filmes(id_filme)
+);
+
+CREATE TABLE filmes (
+    id_filme INT PRIMARY KEY,
+    titulo VARCHAR(15) NOT NULL,
+    ano DATE NOT NULL,
+    sinopse VARCHAR(100) NOT NULL,
+    tempo VARCHAR(5) NOT NULL,
+    categoria VARCHAR(15) NOT NULL,
+    classificacao_idade VARCHAR(10) NOT NULL
+);
