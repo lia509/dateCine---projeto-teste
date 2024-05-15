07297a4cfbaad93d2d7a10020813a35c840ae8a0
Giulia Marques <giuliamarques.564@gmail.com>
2024-05-15 14:48:26 -0300

    alteração index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const filmesRoutes = require('./routes/filmesRoutes');
 const produtoresRoutes = require('./routes/produtoresRoutes');
-const contatoRoutes = require('./routes/produtoresRoutes');
+const contatosRoutes = require('./routes/contatosRoutes');
 
 const app = express();
 const port = process.env.PORT || 3100;  // Default to 3000 if PORT is not set
@@ -13,7 +13,7 @@ app.use('/', filmesRoutes);
 
 app.use('/', produtoresRoutes);
 
-app.use('/', contatoRoutes);
+app.use('/', contatosRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}🧙🪄✨`);
