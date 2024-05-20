cbcdd77e745e05a8688748e588d3bade32023efb
iam.mattosz <victormaria@aluno.senai.br>
2024-05-20 11:30:09 -0300

    api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const filmesRoutes = require('./routes/filmesRoutes');
-
+const cors = require('cors');
 const contatosRoutes = require('./routes/contatosRoutes');
 
 const app = express();
-const port = process.env.PORT || 3100;  // Default to 3000 if PORT is not set
-
+const port = process.env.PORT || 3000;  
+app.use(cors({ origin: 'http://localhost:8081' }));
 app.use(express.json());
 
 app.use('/', filmesRoutes);
 
-app.use(cors({ origin: 'http://localhost:8081' }));
+
 
 app.use('/', contatosRoutes);
 
